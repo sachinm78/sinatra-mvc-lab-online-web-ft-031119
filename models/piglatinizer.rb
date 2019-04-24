51ee057dc9e3728dd270e8215c70032f005b232e
Sachin Mahajan <sachinm78@gmail.com>
2019-04-24 17:39:12 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,27 +1,21 @@
 class PigLatinizer
 
-   def piglatinize(word)
-    return word if %w[and an in].include?(word) #one syllable exceptions
-    letters = word.split("")
-    letters.keep_if {|letter| letter != "."}
-    if letters.size > 1
-      until vowel?(letters[0]) 
-        letters << letters.shift
-      end
-      letters  << "ay"
+  def is_vowel?(word)
+    vowels = 'aeiouAEIOU'
+    vowels.include?(word[0])
+  end
+
+  def piglatinize(word)
+    if self.is_vowel?(word)
+      word.concat('way')
+    else
+      word.concat(word.slice!(/^[^aeiouAEIOU]*/i || "")) + 'ay'
     end
-    letters.join
   end
 
-   def to_pig_latin(text)
-    words = text.split(" ")
-    words.map! {|word| piglatinize(word)}
-    words.join(" ")
+  #if the word starts with a vowel add yay to the end
+  #if the word starts with consonant(s) add the consonants up to the next vowel and ay to the end
+  def to_pig_latin(phrase)
+    phrase.split.collect {|word| piglatinize(word)}.join(' ')
   end
-
-   def vowel?(letter)
-    letter.downcase
-    letter == "o" || letter == "e" || letter == "a" || letter == "i" || letter == "u"
-  end
-
- end 
+end
